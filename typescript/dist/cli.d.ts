7d40f7200f5838c61dbe92831ce7a364969d3848
evilgon <harran39318@gmail.com>
2023-06-13 10:18:38 -0700

    fixed gitigonre

--- typescript/dist/cli.d.ts
+++
@@ -1,7 +0,0 @@
-import { AptosAccount, AptosClient } from "aptos";
-import { Command } from "commander";
-export declare const readConfig: (program: Command) => {
-    client: AptosClient;
-    account: AptosAccount;
-};
-//# sourceMappingURL=cli.d.ts.map
